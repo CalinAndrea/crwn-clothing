2a5ef46231e9ce47695b7912706995625a9ac2e3
Andrea Calin <andrea.n.calin@gmail.com>
2020-05-16 15:35:25 +0200

    dropdown small visual fix

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,8 +6,8 @@ import './cart-dropdown.styles.scss'
 const CartDropdown = () => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
-            <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
